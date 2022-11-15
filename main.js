bb79802835625ac9546af52dfeb96ea1b785bccf
juan <jjhuertasbotache@gmail.com>
2022-11-15 14:49:43 -0500

    update 15/11

--- main.js
+++ main.js
@@ -23,6 +23,8 @@ var ground = document.querySelector(".ground");
 var dinosaur = document.querySelector(".dinosaur");
 var score = document.querySelector(".score");
 
+var cactusList = []
+
 var groundPos = 0;
 var groundWidth = ground.offsetWidth;
 
@@ -31,6 +33,8 @@ var dinoPos = 10
 var dinoVelx = 15;
 
 const gravity = 4;
+
+
 
 /**
  * Moves the ground, and returns it when it finish
@@ -42,7 +46,7 @@ function move_ground() {
     else{
         groundPos=0;
     }
-    ground.style.left = groundPos + "px"
+    ground.style.left = groundPos + "px";
 }
 
 function velYRender() {
@@ -53,11 +57,52 @@ function velYRender() {
     if (dinoPos<=10) {dinoPos = 10; dinoVely = 0;}
 }
 
-var cactus = document.createElement("div");
-cactus.classList.add("cactus");
-document.querySelector(".container").appendChild(cactus);
-cactus.style.right = 0 - cactus.offsetWidth + "px";
-cactus.style.bottom = 0 ;
+/**
+ * class to  create and use the cactus objects
+ */
+class cactus{
+    constructor(){
+        this.create();
+        this.pos = 0;
+        this.reposicionate();
+    }
+    
+    create(){
+        this.obj = document.createElement("div");
+        this.obj.classList.add("cactus");
+        document.querySelector(".container").appendChild(this.obj);
+        this.obj.style.bottom = 0;
+        console.log("cactus created");
+
+    }
+
+    reposicionate (){
+        this.obj.style.right = 0 + (this.obj.offsetWidth * -1) + "px";
+        this.pos = 0;
+        console.log(this.obj.style.right);
+        console.log("re posicionated");
+    }
+    update_pos(){
+        this.obj.style.right = this.pos + "px";
+    }
+}
+
+
+/**
+ * function that moves all the cactus
+ */
+function move_cactus() {
+    cactusList.forEach((cactus) => {
+        if (cactus.pos < (document.querySelector(".container").clientWidth)) {
+            cactus.pos+=dinoVelx;
+        }
+        else{cactus.pos = 0}
+        cactus.update_pos();
+    })
+    
+}
+
+
 
 // main functions--------------------------------------
 /**
@@ -70,15 +115,19 @@ function start(){
             // event.key === "ArrowUp" ||
             event.key == " " 
             ) {
-            if(dinoPos<=10) {dinoVely += 40;}
+                if(dinoPos<=10) {dinoVely += 40;}
+            }
         }
-        
-    });
-
+        );
+        cactusList.push(new cactus());    
+        console.log(cactusList);
 }
-
+/**
+ * This function is repeated each frame
+ */
 function loop() {
     move_ground();
+    move_cactus();
     velYRender();
 }
 
